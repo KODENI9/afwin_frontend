57c0606859c174ad9a2299d9b3cba0121269a5f0
KODENI9 <t29077864@gmail.com>
2026-04-09 22:59:00 +0000

    feat: configure PWA support using vite-plugin-pwa and generate service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.6l4b2qkcb2o"
+    "revision": "0.m252dav8jag"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
